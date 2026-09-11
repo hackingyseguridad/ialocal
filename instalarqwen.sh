70d7f7faed5bf0d75b75551a7ad1ac18c8729d64
hackingyseguridad <antonio_taboada@hotmail.es>
2026-09-11 22:03:14 +0200

    Update instalarqwen.sh

--- instalarqwen.sh
+++ instalarqwen.sh
@@ -1,13 +1,12 @@
-
-
-# Instalar Qwen
-
-
-# qwencli - Qwen - qwencli - zsh (qterm) - node - 80x24
-
+#!/bin/sh
+# (R) Antonio Taboada - hackingyseguridad.com 2026 
+# Script para instalar Qwen y poder ejecutar en local IA
+echo "Instalando herramientas para hacking con IA ... "  
+echo "(R) hackingyseguridad.com 2026 "
+chmod 777 *
 
 
 curl -fsSL https://qwen-code-assets.oss-cn-hangzhou.aliyuncs.com/installation/install-qwen-standalone.sh | bash
 
 
-
+# qwencli - Qwen - qwencli - zsh (qterm) - node - 80x24
