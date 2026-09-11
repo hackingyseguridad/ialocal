539a7b34e422d2e652fce66e005473c8d9c11279
hackingyseguridad <antonio_taboada@hotmail.es>
2026-09-11 20:38:38 +0200

    Create ollama.sh

---
+++ ollama.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# IA en Local sin GPU offline !
+# @antonio_taboada - hackingyseguridad.com - 2026
+
+echo " "
+echo "IA en Local sin GPU !"
+echo "ejecuta deepseek-r1:1.5b de ollama en LOCAL offline, con opencode "
+echo " "
+echo "Pentesting con IA local: sin mandar datos fuera. Ollama + modelos abiertos + Kali/OpenCode, viendo qué se puede hacer realmente offline y qué se pierde frente a modelos potentes en la nube. Para un entorno corporativo, el debate de privacidad puede dar muchísimo juego."
+echo
+
+# otros modelos similares en LOCAL y sin GPU !! solo CPU
+# ollama launch opencode --model deepseek-r1:1.5b
+# ollama launch opencode --model monotykamary/whiterabbitneo-v1.5a 
+# ollama launch opencode --model captainkyd/whiterabbitneo7b
+# ollama launch opencode --model lazarevtill/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B
+#
+
+# ollama launch claude --model deepseek-r1:1.5b
+# ollama launch claude --model monotykamary/whiterabbitneo-v1.5a 
+# ollama launch claude --model captainkyd/whiterabbitneo7b
+# ollama launch claude --model lazarevtill/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B
+#
+
+# ollama launch claude --model deepseek-r1:1.5b
+# ollama --model deepseek-r1:1.5b
+# ollama launch opencode --model deepseek-r1:1.5b
