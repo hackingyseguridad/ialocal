64a983595f51a981ba9a02729a2112c3f1b936c7
hackingyseguridad <antonio_taboada@hotmail.es>
2026-09-11 20:45:22 +0200

    Update instalarqwen.sh

--- instalarqwen.sh
+++ instalarqwen.sh
@@ -2,6 +2,11 @@
 
 # Instalar Qwen
 
+
+# qwencli - Qwen - qwencli - zsh (qterm) - node - 80x24
+
+
+
 curl -fsSL https://qwen-code-assets.oss-cn-hangzhou.aliyuncs.com/installation/install-qwen-standalone.sh | bash
 
 
