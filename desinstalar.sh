3defe173b7a13fb670633f53405f6efc3b05d395
hackingyseguridad <antonio_taboada@hotmail.es>
2026-09-12 23:05:37 +0200

    Create desinstalar.sh

---
+++ desinstalar.sh
@@ -0,0 +1,11 @@
+
+# Eliminar Ollama
+
+ollama logout 
+sudo systemctl stop ollama
+sudo systemctl disable ollama
+sudo rm /etc/systemd/system/ollama.service
+sudo rm $(which ollama)
+sudo rm -r /usr/share/ollama
+sudo userdel ollama
+sudo groupdel ollama
