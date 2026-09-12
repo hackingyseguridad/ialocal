7486e6743415b48f327277698f24c0a069bdad31
hackingyseguridad <antonio_taboada@hotmail.es>
2026-09-12 22:27:07 +0200

    Create opencode.sh

---
+++ opencode.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# (R) hackingyseguridad.com 2026
+# Script Bash Shell 1.0.x para ejecutar interface OpenCode, con ollana, impulsado por otro modelo offline
+# Tenemos que instalar Ollama
+#  curl -fsSL https://ollama.com/install.sh | sh
+# Tenemos que instalar primero OpenCode, con API Gratuita, sin saldo ni inscripcion
+#  curl -fsSL https://claude.ai/install.sh | bash
+echo
+echo "... "
+echo
+ollama -v
+echo " "
+cat << 'EOF'
+                                   ▄
+  █▀▀█ █▀▀█ █▀▀█ █▀▀▄ █▀▀▀ █▀▀█ █▀▀█ █▀▀█
+  █  █ █  █ █▀▀▀ █  █ █    █  █ █  █ █▀▀▀
+  ▀▀▀▀ █▀▀▀ ▀▀▀▀ ▀▀▀▀ ▀▀▀▀ ▀▀▀▀ ▀▀▀▀ ▀▀▀▀
+
+EOF
+echo
+~/.local/bin/./claude -V
+claude doctor
+echo
+echo "Interface OPENCODE , impulsado por otro modelo online:cloud: -- hackingyseguridad.com -- v1.0 "
+echo "/"
+export LANG=es_US.UTF-8
+export LC_ALL=es_ES.UTF-8
+sleep 3
+
+# CLaude Code impulsado por deepseek-r1:1.5b offline!!!
+ollama launch opencode --model deepseek-r1:1.5b
