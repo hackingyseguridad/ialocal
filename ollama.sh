abd25fa87e07d7805386da12a44bb8041cebd5af
hackingyseguridad <antonio_taboada@hotmail.es>
2026-09-12 22:07:52 +0200

    Update ollama.sh

--- ollama.sh
+++ ollama.sh
@@ -9,7 +9,6 @@ echo " "
 echo "Pentesting con IA local: sin mandar datos fuera. Ollama + modelos abiertos + Kali/OpenCode, viendo qué se puede hacer realmente offline y qué se pierde frente a modelos potentes en la nube. Para un entorno corporativo, el debate de privacidad puede dar muchísimo juego."
 echo
 
-
 # MODELOS DE OLLAMA en LOCAL combinados con herramienta OpenCode y sin GPU !! solo CPU
 # ------------------------------------------------------------------------------------
 # ollama launch opencode --model deepseek-r1:1.5b
@@ -17,14 +16,12 @@ echo
 # ollama launch opencode --model captainkyd/whiterabbitneo7b
 # ollama launch opencode --model lazarevtill/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B
 
-
 # MODELOS DE OLLAMA en LOCAL combinados con herramienta OpenCode y sin GPU !! solo CPU
 # ------------------------------------------------------------------------------------
 # ollama launch claude --model deepseek-r1:1.5b
 # ollama launch claude --model monotykamary/whiterabbitneo-v1.5a 
 # ollama launch claude --model captainkyd/whiterabbitneo7b
 # ollama launch claude --model lazarevtill/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B
-
 
 # MODELOS DE OLLAMA DIRECTAEMENTE EN LOCAL, 
 # -----------------------------------------------
