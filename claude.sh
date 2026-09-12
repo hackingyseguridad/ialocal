ae61ec1f813b30ed859379aad9bd3562e1a121e5
hackingyseguridad <antonio_taboada@hotmail.es>
2026-09-12 21:58:11 +0200

    Create claude.sh

---
+++ claude.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# (R) hackingyseguridad.com 2026
+# Script Bash Shell 1.0.x para ejecutar interface Claude Code, con ollana, impulsado por otro modelo offline
+# Tenemos que instalar Ollama
+#  curl -fsSL https://ollama.com/install.sh | sh
+# Tenemos que instalar primero Claude , con API Gratuita, sin saldo ni inscripcion
+#  curl -fsSL https://claude.ai/install.sh | bash
+echo
+echo "... "
+echo
+ollama -v
+echo " "
+cat << 'EOF'
+ ██████  ██       █████  ██    ██ ██████  ███████
+██       ██      ██   ██ ██    ██ ██   ██ ██
+██       ██      ███████ ██    ██ ██   ██ █████
+██       ██      ██   ██ ██    ██ ██   ██ ██
+ ██████  ███████ ██   ██  ██████  ██████  ███████
+
+ ██████  ██████  ██████  ███████
+██       ██   ██ ██   ██ ██
+██       ██   ██ ██   ██ █████
+██       ██   ██ ██   ██ ██
+ ██████  ██████  ██████  ███████
+EOF
+echo
+~/.local/bin/./claude -V
+claude doctor
+echo
+echo "Interface CLAUDE CODE , impulsado por otro modelo online:cloud: -- hackingyseguridad.com -- v1.0 "
+echo "/"
+export LANG=es_US.UTF-8
+export LC_ALL=es_ES.UTF-8
+sleep 3
+
+# CLaude Code impulsado por Gemma3 offline!!!
+ollama launch claude --model gemma3:1b
