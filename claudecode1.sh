c863e3033607b692e26f8e20625ec0893072f3bf
hackingyseguridad <antonio_taboada@hotmail.es>
2026-09-20 22:43:40 +0200

    Revise launch options for Claude Code script
    Updated comments and command options for launching Claude Code.

--- claudecode1.sh
+++ claudecode1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # (R) hackingyseguridad.com 2026
-# Script Bash Shell 1.0.x para ejecutar interface Claude Code, con ollana, impulsado por otro modelo online:cloud
+# Script Bash Shell 1.0.x para ejecutar interface Claude Code, con ollana, impulsado por otro modelo oFFLINE
 # Tenemos que instalar Ollama
 #  curl -fsSL https://ollama.com/install.sh | sh
 # Tenemos que instalar primero Claude , con API Gratuita, sin saldo ni inscripcion
@@ -32,23 +32,11 @@ echo "/"
 export LANG=es_US.UTF-8
 export LC_ALL=es_ES.UTF-8
 sleep 3
-# Opcion0
-# ollama launch claude --config
-# escogemos una opcion para ejecutar claude code online o local impulsado
 
-# Opcion1
-# Claude Code Impulsado por gpt-oss:20b-cloud online en la nube !!!
-# ollama launch claude --model  gpt-oss:20b-cloud
+# ollama launch claude --model  gpt-oss:20b
 
-# Opcion2
-# CLaude Code impulsado por Gemma4 online!!!
-ollama launch claude --model gemma4:31b-cloud
+ollama launch claude --model gemma4:31b
 
-# Opcion3
-# Claude Code impuslado por minimax-m3:cloud   - la mas rapida!!
-# ollama launch claude --model minimax-m3:cloud
+# ollama launch claude --model minimax-m3
 
-# Opcion4
-# ejecuta Claude (sin GPU NVIDIA)
-# Claude Code impulsado por Qwen en local PC
 # ollama launch claude --model qwen3-coder
